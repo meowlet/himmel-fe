9b37f75c00e90724f7aef18bb3f038fcfc82e6e7
Te1ne <nq2019thaigiabao230504@gmail.com>
2024-10-19 00:00:07 +0700

    chore: Update project configuration
    - Add osu.ppy.sh to allowed image hosts
    - Ignore ESLint during builds
    - Update start script to use port 3001

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "localhost" }],
+    remotePatterns: [{ hostname: "localhost" }, { hostname: "osu.ppy.sh" }],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
